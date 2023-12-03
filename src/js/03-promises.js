71b1a9cb6294024a3fcc99f8d5d7460d7fc70855
Calin Daria Andreea <calindariaandreea@gmail.com>
2023-12-03 20:01:16 +0200

    .

---
+++ src/js/03-promises.js
@@ -0,0 +1,39 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const firstDelay = parseInt(form.elements.delay.value);
+  const delayStep = parseInt(form.elements.step.value);
+  const amount = parseInt(form.elements.amount.value);
+
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, firstDelay + (i - 1) * delayStep)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+});
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
